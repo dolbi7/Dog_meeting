98675a4e605f40769e62cd84d09c2ecd23475197
dolbi7 <freefawn0@gmail.com>
2025-03-01 14:50:24 +0900

    test

---
+++ src/main/java/com/example/dog_meeting/DogMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.dog_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogMeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dog_meeting/DogMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dog_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
